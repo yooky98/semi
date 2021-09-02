5312287c498591eac5ce833a8d8bb6971e514e37
JinSeokJang91 <85108311+JinSeokJang91@users.noreply.github.com>
2021-09-02 19:34:11 +0900

    제품 등록, 수정(미완)
    aa

--- jspPro1/src/com/kh/admin/adminProduct/model/service/adminProductService.java
+++ jspPro1/src/com/kh/admin/adminProduct/model/service/adminProductService.java
@@ -1,12 +1,15 @@
 package com.kh.admin.adminProduct.model.service;
 import static com.kh.common.JDBCTemplate.close;
+import static com.kh.common.JDBCTemplate.commit;
 import static com.kh.common.JDBCTemplate.getConnection;
+import static com.kh.common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
 import java.util.ArrayList;
 
 import com.kh.admin.adminProduct.model.dao.adminProductDao;
 import com.kh.admin.adminProduct.model.vo.adminProduct;
+import com.kh.product.model.vo.Attachment;
 
 public class adminProductService {
 
@@ -19,6 +22,50 @@ public class adminProductService {
 		close(conn);
 		
 		return list;		
+	}
+
+	public int insertProduct(adminProduct ap, Attachment at) {
+		
+		Connection conn = getConnection();
+		
+		int result1 = new adminProductDao().insertProduct(conn, ap);
+		int result2 = 1;
+		
+		if(at != null) {
+			result2 = new adminProductDao().insertAttachment(conn, at);
+		}
+		
+		if(result1*result2 > 0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result1*result2;
+	}
+
+	public adminProduct selectUpdateProduct(int prodNo) {
+		
+		Connection conn = getConnection();
+		
+		adminProduct ap = new adminProductDao().selectProduct(conn, prodNo);
+		
+		close(conn);
+		
+		return ap;
+	}
+
+	public Attachment selectAttachment(int prodNo) {
+
+		Connection conn = getConnection();
+		
+		Attachment at = new adminProductDao().selectAttachment(conn, prodNo);
+		
+		close(conn);
+		
+		return at;
 	}
 	
 	
